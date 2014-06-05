3e6bfb086ae663fa58a9063557a9356d08a409df
Luke Ashford <luke_a_ashford@yahoo.co.uk>
2014-06-05 07:44:13 +0100

    Include WhistPlayer

---
+++ CardGames/Whist/WhistPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardsLibrary;
+
+namespace CardGames.Whist
+{
+    public abstract class WhistPlayer : Player<WhistInfo>
+    {
+        protected List<Card> DetectValidCards(WhistInfo args)
+        {
+            // Can the hand follow suit
+            bool followSuit = Hand.Any(c => c.Suit == args.FirstSuitLaid);
+
+            if(followSuit)
+            {
+                // If the hand can follow suit, return only cards of the correct suit
+                return Hand.Where(c => c.Suit == args.FirstSuitLaid).ToList();
+            }
+            else
+            {
+                // Else return all cards
+                return Hand;
+            }
+        }
+    }
+}
