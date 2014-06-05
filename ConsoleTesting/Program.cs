f86ed93ce98d8056aa569903976e8828d6b6086b
It423 <it42@hotmail.co.uk>
2014-06-05 15:56:31 +0100

    add ConsolePlayer Testing

---
+++ ConsoleTesting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardGames;
+using CardsLibrary;
+
+namespace ConsoleTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Card> Deck;
+            CardFactory.PopulateDeck(out Deck, true);
+
+            List<CardGames.Whist.ConsolePlayer> Players = new List<CardGames.Whist.ConsolePlayer>();
+
+            Card[][] tempPlayers = new Card[2][];
+
+            CardFactory.Deal(ref Deck, ref tempPlayers, 7);
+
+            Players.Add(new CardGames.Whist.ConsolePlayer(tempPlayers[0]));
+            Players.Add(new CardGames.Whist.ConsolePlayer(tempPlayers[1]));
+
+            CardGames.Whist.WhistInfo GameInfo = new CardGames.Whist.WhistInfo(new List<Card>(), Suit.Clubs, Suit.Null);
+
+            Players[0].MakeMove(GameInfo);
+            Players[1].MakeMove(GameInfo);
+        }
+    }
+}
