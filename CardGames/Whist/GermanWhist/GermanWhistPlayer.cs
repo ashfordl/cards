c1d98fc4a378e8cfe239ea54a2f5ce6090076388
It423 <it42@hotmail.co.uk>
2014-07-27 14:16:27 +0100

    Make the orderPlayers method abstract in the base class

--- CardGames/Whist/GermanWhist/GermanWhistPlayer.cs
+++ CardGames/Whist/GermanWhist/GermanWhistPlayer.cs
@@ -32,5 +32,47 @@ namespace CardGames.Whist.GermanWhist
                 return this.Hand;
             }
         }
+
+        /// <summary>
+        /// Orders the hand.
+        /// </summary>
+        protected override void OrderCards()
+        {
+            // A list of a list of cards of one suit
+            List<List<Card>> orderedBySuit = new List<List<Card>>(4);
+
+            // Check through each suit
+            for (int i = 0; i < 4; i++)
+            {
+                // Create a new item in the list for the suit
+                orderedBySuit.Add(new List<Card>());
+
+                // Check through each card in the hand
+                foreach (Card c in this.Hand)
+                {
+                    // If the suit is equal to the one being looked for add it to the list
+                    if (c.Suit == (Suit)i + 1)
+                    {
+                        orderedBySuit[i].Add(c);
+                    }
+                }
+            }
+
+            // Clear the hand
+            this.Hand.Clear();
+
+            // Iterate through each list of a suit
+            for (int i = 0; i < 4; i++)
+            {
+                // While there are cards in the list
+                while (orderedBySuit[i].Count != 0)
+                {
+                    // Pick the lowest card and add it to the hand
+                    Card lowest = Card.LowestCardFromArray(orderedBySuit[i]);
+                    orderedBySuit[i].Remove(lowest);
+                    this.Hand.Add(lowest);
+                }
+            }
+        }
     }
 }
